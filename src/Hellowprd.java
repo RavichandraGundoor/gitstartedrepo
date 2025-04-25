7bb7324599c9dd26eff7740df1e5bfc1ccc716af
ravi <ravicreed1@gmail.com>
2025-04-25 21:04:51 +0530

    4th  commit

--- src/Hellowprd.java
+++ src/Hellowprd.java
@@ -2,6 +2,10 @@ public class Hellowprd {
     public static void main(String[] arg){
         System.out.println("HI");
         System.out.println("hello");
+        public void display(){
+
+        }
+
 
 
     }
